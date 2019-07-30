40500d48af772a84e293b06c46866f9b662cde19
xystus45 <xystusngigi@gmail.com>
2019-07-30 12:12:04 +0300

    insert user list

--- locker.py
+++ locker.py
@@ -1,3 +1,13 @@
 import pyperclip
 import random
-import string 
+import string
+
+global users_list
+
+class User:
+    '''
+    class to create user accounts and save their information
+    '''
+    # class Variables
+    # global users_list
+    users_list = []
